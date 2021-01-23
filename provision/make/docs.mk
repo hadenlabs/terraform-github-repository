123ea11eafb0e381c19c05f701d7253fb86db112
@slovacus <slovacus@gmail.com>
2021-01-23 17:33:03 -0500

    refactor: change docs rst to markdown using mkdocs (#9)

--- provision/make/docs.mk
+++ provision/make/docs.mk
@@ -1,8 +1,6 @@
 #
-# See ./CONTRIBUTING.rst
+# See ./docs/contributing.md
 #
-
-FILE_README=$(ROOT_DIR)/README.md
 
 docs:
 	make docs.help
@@ -10,8 +8,8 @@ docs:
 docs.help:
 	@echo '    Docs:'
 	@echo ''
-	@echo '        docs.show                  Show restview README'
-	@echo '        docs.make                  Make documentation html'
+	@echo '        docs.build                  Show mkdocs'
+	@echo '        docs.serve                  server Make documentation'
 	@echo '        docs.terraform             generated docs for terraform'
 	@echo ''
 
@@ -20,8 +18,8 @@ docs.terraform:
 			'This document gives an overview of variables used in the platform of the ${PROJECT}.', \
 			variables.tf)
 
-docs.show:
-	$(PIPENV_RUN) restview ${FILE_README}
+docs.build:
+	$(PIPENV_RUN) mkdocs build
 
-docs.make:
-	$(docker-compose) -f ${PATH_DOCKER_COMPOSE}/dev.yml run --rm docs bash -c "cd docs && make html"
+docs.serve:
+	$(PIPENV_RUN) mkdocs serve
