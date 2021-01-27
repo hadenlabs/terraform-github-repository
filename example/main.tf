ba9810a10516bcafdb665f03c9dd8592ac40e76b
Luis Mayta <luis@hadenlabs.com>
2021-01-27 04:00:19 -0500

    sample: module with key public (#12)

--- example/main.tf
+++ example/main.tf
@@ -7,3 +7,15 @@ module "main" {
   description = var.repository_description
   visibility  = "public"
 }
+
+module "main_with_key" {
+  providers {
+    github = github
+  }
+  source      = "git://github.com/hadenlabs/terraform-github-repository.git?ref=0.1.0"
+  name        = var.repository_name
+  description = var.repository_description
+  visibility  = "public"
+  key         = "/usr/etc/key/user.pub"
+  read_only   = false
+}
