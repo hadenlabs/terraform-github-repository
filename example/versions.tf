33f30395616523671dafd66830d840642b232be7
Luis Mayta <luis@hadenlabs.com>
2021-02-12 14:34:19 -0500

    sample: fix implement provider (#17)

--- example/versions.tf
+++ example/versions.tf
@@ -1,9 +1,10 @@
 terraform {
   required_version = ">=0.13.0"
   required_providers {
+
     github = {
       source  = "integrations/github"
-      version = ">=2.9.2"
+      version = ">=4.3.0"
     }
 
   }
