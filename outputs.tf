7573de9a37d0bca4567c6c09df5d88a6f5d9919c
Luis Mayta <luis@hadenlabs.com>
2021-02-14 09:31:38 -0500

    chore: implement github issue label (#20)

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "instance" {
   description = "output instance repository"
-  value       = github_repository.instance
+  value       = github_repository.this
 }
 
 output "deploy_key" {
   description = "output instance deploy_key"
-  value       = github_repository_deploy_key.instance
+  value       = github_repository_deploy_key.this
 }
