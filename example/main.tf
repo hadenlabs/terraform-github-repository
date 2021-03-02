4d17f4c824837774a46c787c8ed82f784874504a
@slovacus <slovacus@gmail.com>
2021-03-02 14:54:09 -0500

    build: generate version patch (0.7.2)

--- example/main.tf
+++ example/main.tf
@@ -2,7 +2,7 @@ module "main" {
   providers = {
     github = github
   }
-  source      = "git://github.com/hadenlabs/terraform-github-repository.git?ref=0.7.1"
+  source      = "git://github.com/hadenlabs/terraform-github-repository.git?ref=0.7.2"
   name        = var.repository_name
   description = var.repository_description
   visibility  = "public"
@@ -12,7 +12,7 @@ module "main_with_key" {
   providers = {
     github = github
   }
-  source      = "git://github.com/hadenlabs/terraform-github-repository.git?ref=0.7.1"
+  source      = "git://github.com/hadenlabs/terraform-github-repository.git?ref=0.7.2"
   name        = var.repository_name
   description = var.repository_description
   visibility  = "public"
