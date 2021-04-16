eb2ca3289becd3ded7637aafbc9a96b773f68dc2
@slovacus <slovacus@gmail.com>
2021-04-16 17:59:30 -0500

    style: format code typescript (#63)

--- provision/generators/plop/utils/index.ts
+++ provision/generators/plop/utils/index.ts
@@ -1,2 +1,9 @@
-import { hyphenate, baseGeneratorPath, pathExists, pathMake, baseTemplatesPath, cleanString } from './helpers'
+import {
+  hyphenate,
+  baseGeneratorPath,
+  pathExists,
+  pathMake,
+  baseTemplatesPath,
+  cleanString,
+} from './helpers'
 export { hyphenate, baseGeneratorPath, pathExists, pathMake, baseTemplatesPath, cleanString }
