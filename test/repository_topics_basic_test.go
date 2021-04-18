25b4e0aab3c1e043ccf0355fc8dd2f3f13feda26
@slovacus <slovacus@gmail.com>
2021-04-18 03:29:14 -0500

    style: format code go (#65)

--- test/repository_topics_basic_test.go
+++ test/repository_topics_basic_test.go
@@ -8,34 +8,34 @@ import (
 )
 
 func TestTopicsBasicSuccess(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 
-  visibility := "public"
-  repository := "test-repository"
-  description := "description"
-  types := []string{"flask"}
-  topics:= []string{"python"}
+	visibility := "public"
+	repository := "test-repository"
+	description := "description"
+	types := []string{"flask"}
+	topics := []string{"python"}
 
-      terraformOptions := &terraform.Options{
-            //The path to where your Terraform code is located
-            TerraformDir: "repository-topics-basic",
-            Upgrade:      true,
-            Vars: map[string]interface{}{
-              "visibility":  visibility,
-              "name":        repository,
-              "description": description,
-              "types": types,
-              "topics": topics,
-            },
-    }
+	terraformOptions := &terraform.Options{
+		//The path to where your Terraform code is located
+		TerraformDir: "repository-topics-basic",
+		Upgrade:      true,
+		Vars: map[string]interface{}{
+			"visibility":  visibility,
+			"name":        repository,
+			"description": description,
+			"types":       types,
+			"topics":      topics,
+		},
+	}
 
-    // At the end of the test, run `terraform destroy` to clean up any resources that were created
-    defer terraform.Destroy(t, terraformOptions)
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
 
-    // This will run `terraform init` and `terraform apply` and fail the test if there are any errors
-    terraform.InitAndApply(t, terraformOptions)
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
 
-    outputTopics := terraform.OutputRequired(t, terraformOptions, "topics")
-    // Verify we're getting back the outputs we expect
-    assert.NotEmpty(t, outputTopics, outputTopics)
+	outputTopics := terraform.OutputRequired(t, terraformOptions, "topics")
+	// Verify we're getting back the outputs we expect
+	assert.NotEmpty(t, outputTopics, outputTopics)
 }
