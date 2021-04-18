33a7bb3376d908712b911848141586177d114115
Luis Mayta <luis@hadenlabs.com>
2021-04-18 03:17:44 -0500

    test: implement labels default (#65)

---
+++ test/repository_add_labels_default_test.go
@@ -0,0 +1,39 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddLabelsDefaultSuccess(t *testing.T) {
+	t.Parallel()
+
+	name := "repository-test"
+	visibility := "public"
+  description := "description"
+	add_labels_default := true
+
+	terraformOptions := &terraform.Options{
+		//The path to where your Terraform code is located
+		TerraformDir: "repository-add-labels-default",
+		Upgrade:      true,
+		Vars: map[string]interface{}{
+			"name":               name,
+			"visibility":         visibility,
+			"add_labels_default": add_labels_default,
+      "description": description,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+	outputRepository := terraform.Output(t, terraformOptions, "instance")
+	outputLabels := terraform.Output(t, terraformOptions, "labels")
+	assert.NotEmpty(t, outputRepository, outputRepository)
+	assert.NotEmpty(t, outputLabels, outputLabels)
+}
