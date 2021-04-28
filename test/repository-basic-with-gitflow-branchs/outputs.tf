b3790df62a18cb050ca3bc865cbe4887e2a113ba
Luis Mayta <luis@hadenlabs.com>
2021-04-27 19:43:15 -0500

    test: implement validation create gitflow (#69)

---
+++ test/repository-basic-with-gitflow-branchs/outputs.tf
@@ -0,0 +1,9 @@
+output "instance" {
+  description = "show instance module"
+  value       = module.main.instance
+}
+
+output "repository_name" {
+  description = "show name repository"
+  value       = module.main.name
+}
