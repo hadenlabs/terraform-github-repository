a14954e3f445b9ed6ed1a7d151c54fdab4ce1d7c
luis mayta <slovacus@gmail.com>
2021-08-05 20:44:20 -0500

    refactor: local output generation (#75)

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 locals {
 
-  defaults = {
-    topics = yamldecode(file("${path.module}/data/topics.yaml"))
-    labels = yamldecode(file("${path.module}/data/labels.yaml"))
+  default = {
+    topics                   = yamldecode(file("${path.module}/data/topics.yaml"))
+    labels                   = yamldecode(file("${path.module}/data/labels.yaml"))
+    branch_name_exclude_list = ["main", "develop", "/"]
     settings = {
       auto_init              = false
       has_issues             = true
@@ -21,6 +22,13 @@ locals {
       vulnerability_alerts   = true
       template               = {}
     }
+
+    pages = {
+      branch = var.default_branch
+      path   = "/"
+      cname  = ""
+    }
+
   }
 
   input = {
@@ -28,25 +36,24 @@ locals {
     types              = try(var.types, [])
     topics             = try(var.topics, [])
     add_labels_default = try(var.add_labels_default, false)
+    pages              = try(var.pages, {})
+  }
+
+  generated = {
+    pages = try(local.input.pages, local.default.pages)
   }
 
   generated_topics = flatten([
-    for k in local.input.types : tolist(lookup(local.defaults.topics, k))
+    for k in local.input.types : tolist(lookup(local.default.topics, k))
   ])
+  generated_labels = tolist(lookup(local.default.labels, "labels"))
 
-  generated_labels = tolist(lookup(local.defaults.labels, "labels"))
-
-  outputs = {
+  output = {
     add_labels_default = local.input.add_labels_default
-    settings           = merge(local.defaults.settings, local.input.settings)
+    settings           = merge(local.default.settings, local.input.settings)
     topics             = distinct(flatten(concat(local.generated_topics, local.input.topics)))
     labels             = local.generated_labels
-  }
-
-  default_pages = {
-    branch = var.default_branch
-    path   = "/"
-    cname  = ""
+    pages              = local.generated.pages
   }
 
 }
@@ -57,38 +64,38 @@ resource "github_repository" "this" {
   #checkov:skip=CKV_GIT_1:The resource github is module
   visibility = var.visibility
 
-  homepage_url           = local.outputs.settings.homepage_url
-  has_issues             = local.outputs.settings.has_issues
-  has_projects           = local.outputs.settings.has_projects
-  has_wiki               = local.outputs.settings.has_wiki
-  is_template            = local.outputs.settings.is_template
-  allow_merge_commit     = local.outputs.settings.allow_merge_commit
-  allow_squash_merge     = local.outputs.settings.allow_squash_merge
-  allow_rebase_merge     = local.outputs.settings.allow_rebase_merge
-  delete_branch_on_merge = local.outputs.settings.delete_branch_on_merge
-  auto_init              = local.outputs.settings.auto_init
-  gitignore_template     = local.outputs.settings.gitignore_template
-  license_template       = local.outputs.settings.license_template
-  archived               = local.outputs.settings.archived
-  archive_on_destroy     = local.outputs.settings.archive_on_destroy
-  vulnerability_alerts   = local.outputs.settings.vulnerability_alerts
+  homepage_url           = local.output.settings.homepage_url
+  has_issues             = local.output.settings.has_issues
+  has_projects           = local.output.settings.has_projects
+  has_wiki               = local.output.settings.has_wiki
+  is_template            = local.output.settings.is_template
+  allow_merge_commit     = local.output.settings.allow_merge_commit
+  allow_squash_merge     = local.output.settings.allow_squash_merge
+  allow_rebase_merge     = local.output.settings.allow_rebase_merge
+  delete_branch_on_merge = local.output.settings.delete_branch_on_merge
+  auto_init              = local.output.settings.auto_init
+  gitignore_template     = local.output.settings.gitignore_template
+  license_template       = local.output.settings.license_template
+  archived               = local.output.settings.archived
+  archive_on_destroy     = local.output.settings.archive_on_destroy
+  vulnerability_alerts   = local.output.settings.vulnerability_alerts
 
   topics = var.topics
 
   dynamic "pages" {
-    for_each = length(var.pages) != 0 ? [var.pages] : []
+    for_each = length(local.output.pages) != 0 ? [local.output.pages] : []
 
     content {
       source {
-        branch = lookup(pages.value, "branch", local.default_pages.branch)
-        path   = lookup(pages.value, "path", local.default_pages.path)
+        branch = lookup(pages.value, "branch", local.default.pages.branch)
+        path   = lookup(pages.value, "path", local.default.pages.path)
       }
-      cname = lookup(pages.value, "cname", local.default_pages.cname)
+      cname = lookup(pages.value, "cname", local.default.pages.cname)
     }
   }
 
   dynamic "template" {
-    for_each = length(local.outputs.settings.template) != 0 ? [local.outputs.settings.template] : []
+    for_each = length(local.output.settings.template) != 0 ? [local.output.settings.template] : []
 
     content {
       owner      = lookup(template.value, "owner", null)
@@ -107,6 +114,20 @@ resource "github_repository" "this" {
       is_template,
       vulnerability_alerts,
       description
+    ]
+  }
+}
+
+resource "github_branch" "gh_pages" {
+  count      = !contains(local.default.branch_name_exclude_list, local.output.pages.branch) ? 1 : 0
+  depends_on = [github_repository.this]
+  repository = github_repository.this.name
+  branch     = local.output.pages.branch
+
+  lifecycle {
+    ignore_changes = [
+      branch,
+      source_branch,
     ]
   }
 }
@@ -167,7 +188,7 @@ resource "github_repository_file" "this" {
 
 resource "github_repository_project" "this" {
   depends_on = [github_repository.this]
-  count      = local.outputs.settings.has_projects ? 1 : 0
+  count      = local.output.settings.has_projects ? 1 : 0
   name       = lower(var.name)
   repository = github_repository.this.name
   body       = var.description
@@ -203,9 +224,9 @@ resource "github_repository_collaborator" "this" {
 
 resource "github_issue_label" "this" {
   depends_on  = [github_repository.this]
-  count       = local.outputs.add_labels_default ? length(local.outputs.labels) : 0
+  count       = local.output.add_labels_default ? length(local.output.labels) : 0
   repository  = github_repository.this.name
-  name        = lookup(local.outputs.labels[count.index], "name")
-  description = lookup(local.outputs.labels[count.index], "description")
-  color       = lookup(local.outputs.labels[count.index], "color")
+  name        = lookup(local.output.labels[count.index], "name")
+  description = lookup(local.output.labels[count.index], "description")
+  color       = lookup(local.output.labels[count.index], "color")
 }
