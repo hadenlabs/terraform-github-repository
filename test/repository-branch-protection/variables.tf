8b4cc107253d42dcb89fb20368a0f4ba264207dd
Luis Mayta <luis@hadenlabs.com>
2021-11-01 18:42:51 -0500

    chore: implement module branch protection (#74)

---
+++ test/repository-branch-protection/variables.tf
@@ -0,0 +1,43 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ENVIRONMENT VARIABLES
+# Define these secrets as environment variables.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# GITHUB_OWNER
+# GITHUB_TOKEN
+
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED VARIABLES
+# These variables must be set when using this module.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL VARIABLES
+# These variables have defaults, but may be overridden.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "visibility" {
+  type        = string
+  description = "type of visibility."
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the repository."
+}
+
+variable "description" {
+  type        = string
+  description = "description of repository."
+}
+
+variable "settings" {
+  type        = any
+  description = "settings for github repository."
+}
+
+variable "branch_protection" {
+  type        = map(any)
+  description = "settings branch protection."
+  default     = {}
+}
