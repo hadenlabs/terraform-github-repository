ce51af02d5ece345bac65be26d27290bd5fa2af2
Luis Mayta <luis@hadenlabs.com>
2021-10-31 23:10:20 -0500

    chore: implement common and faker repository (#82)

--- internal/app/external/faker/repository_test.go
+++ internal/app/external/faker/repository_test.go
@@ -3,11 +3,15 @@ package faker
 import (
 	"testing"
 
+	"strings"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestFakeRepositoryName(t *testing.T) {
-	assert.Contains(t, names, Repository().Name())
+	name := Repository().Name()
+	namePrefix := strings.Split(name, "-")[0]
+	assert.Contains(t, name, namePrefix, namePrefix)
 }
 
 func TestFakeRepositoryVisibility(t *testing.T) {
